0e74dd2de4fc728f9a0a33454842d1b4d2aa3411
reco <reco@liao.com>
2015-07-17 17:17:14 +0800

    supplement

---
+++ js/main/components/base/specs/identifiable.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Reco on 2015/7/12.
+ */
+
+//dependencies
+var _ = require('lodash');
+var React = require('react');
+
+//private fields
+var indexMap = {};
+
+//core module to export
+module.exports = {
+    displayName: 'Identifiable',
+    componentWillMount: function() {
+        var className = this.constructor.displayName;
+        var index = _.get(indexMap, className) || 0;
+
+        _.set(indexMap, className, index + 1);
+        this._seq = hyphenFormalize(className) + '-' + index + '-';
+    },
+    getSeq: function() {
+        return this._seq;
+    }
+};
+
+//module initialization
+
+
+//private functions
+function hyphenFormalize(str) {
+    return ('' + str).replace(/[A-Z]/g, function(match, offset) {
+        return (offset ? '-' : '') + match.toLowerCase();
+    })
+}
