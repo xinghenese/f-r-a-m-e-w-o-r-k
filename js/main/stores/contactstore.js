97d0e45113fbd8a4448f0ef67424783645479003
reco <reco@liao.com>
2015-07-27 20:41:51 +0800

    supplement for test

---
+++ js/main/stores/contactstore.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Administrator on 2015/7/27.
+ */
+
+//dependencies
+
+
+//private fields
+
+
+//core module to export
+module.exports = {
+    group: [
+        {name: 'Telegram', count: 12, type: 'group'},
+        {name: 'Designer', count: 20, type: 'group'}
+    ],
+    contact: [
+        {name: 'xue/er', type: 'contact'}
+    ],
+    A: [
+        {name: 'duoduo', type: 'user', online: true, lastActiveTime: null},
+    ],
+    B: [
+        {name: 'TT', type: 'user', online: false, lastActiveTime: 1}
+    ],
+    C: [
+        {name: 'Telegram', type: 'user', online: false, lastActiveTime: 2}
+    ]
+};
+
+//module initialization
+
+
+//private functions
