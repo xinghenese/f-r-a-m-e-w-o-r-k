87634856cbd6cbe95db00649887ea629200c2ed6
sept <sept@liao.com>
2015-07-29 02:59:51 +0800

    add tests task add tasts:tdd task

--- gulpfile.js
+++ gulpfile.js
@@ -64,6 +64,19 @@ gulp.task('clean', function (callback) {
 }());
 // endregion
 
+// region tests
+gulp.task('tests', function (done) {
+    var jest = require('jest-cli');
+    // see http://facebook.github.io/jest/docs/api.html#config-options
+    var options = {};
+    jest.runCLI(options, __dirname, done);
+});
+
+gulp.task('tests:tdd', function () {
+    gulp.watch(['./**/*.js'], ['test']);
+});
+// endregion
+
 gulp.task('local-serve', ['clean'], function () {
     return gulp.src('.')
         .pipe(require('gulp-webserver')());
