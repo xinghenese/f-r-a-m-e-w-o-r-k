7d13a40e8cb810511031766ce96b3567081de99c
reco <reco@liao.com>
2015-07-28 21:10:28 +0800

    cache

--- js/main/stores/contactstore.js
+++ js/main/stores/contactstore.js
@@ -14,13 +14,13 @@ module.exports = {
         //the ideal data shape that ContactList components prefer.
         //or you can wrap the properties like 'count', 'online' and other
         //similar ones under a super-level property like 'info' so as to
-        //make them more uniform. e.g. {name:'', type: '', info:{}}
-        {name: 'Telegram', type: 'group', count: 12},
-        {name: 'Designer', type: 'group', count: 20},
-        {name: 'xue/er', type: 'contact'},
-        {name: 'duoduo', type: 'user', online: true, lastActiveTime: null},
-        {name: 'TT', type: 'user', online: false, lastActiveTime: 1},
-        {name: 'Telegram', type: 'user', online: false, lastActiveTime: 2}
+        //make them more uniform. e.g. {name:'', id:'', type: '', info:{}}
+        {name: 'Telegram', type: 'group', id:'1234', count: 12},
+        {name: 'Designer', type: 'group', id:'2312', count: 20},
+        {name: 'xue/er', type: 'contact', id:'3427'},
+        {name: 'duoduo', type: 'user', id:'7684', online: true, lastActiveTime: null},
+        {name: 'TT', type: 'user', id:'3458', online: false, lastActiveTime: 1},
+        {name: 'Telegram', type: 'user', id:'3782', online: false, lastActiveTime: 2}
     ],
     getContacts: function() {
         //immutable
