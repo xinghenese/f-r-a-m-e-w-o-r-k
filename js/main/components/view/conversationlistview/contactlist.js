0a84097958d3cb9532f75c69871153543987f9a0
reco <reco@liao.com>
2015-08-10 20:54:46 +0800

    reconstruct selectable and groupable datalist

--- js/main/components/view/conversationlistview/contactlist.js
+++ js/main/components/view/conversationlistview/contactlist.js
@@ -7,26 +7,33 @@ var _ = require('lodash');
 var React = require('react');
 var ContactGroup = require('./contactgroup');
 var EventTypes = require('../../../constants/eventtypes');
-var style = require('../../../style/conversationlist');
+var style = require('../../../style/contactlist');
 var makeStyle = require('../../../style/styles').makeStyle;
 var setStyle = require('../../../style/styles').setStyle;
 var emitter = require('../../../utils/eventemitter');
 var Lang = require('../../../locales/zh-cn');
 
+var Avatar = require('../../avatar');
+var createGenerator = require('../../base/creator/createReactClassGenerator');
+var groupableMixin = require('../../base/specs/list/groupable');
+var hoverableMixin = require('../../base/specs/list/hoverable');
+
 //private fields
-var prefix = 'contact-list-';
+var createGroupClass = createGenerator({
+    mixins: [groupableMixin, hoverableMixin]
+});
 
 //core module to export
-var ContactList = React.createClass({
-    getInitialState: function() {
-        return {selectedIndex: -1};
-    },
-    _onSelect: function(data) {
-        this.setState({selectedIndex: data.id});
-        emitter.emit('select', {
-            id: data.id,
-            type: data.type
-        });
+module.exports = createGroupClass({
+    displayName: 'ContactList',
+    getDefaultProps: function () {
+        return {
+            onHoverIn: defaultOnHoverIn,
+            onHoverOut: defaultOnHoverOut,
+            className: "contact-list",
+            style: style.contactlist,
+            groupBy: this.groupBy
+        }
     },
     _selectPreviousContact: function() {
         console.log("selecting previous contact");
@@ -35,6 +42,7 @@ var ContactList = React.createClass({
         console.log("selecting previous contact");
     },
     componentDidMount: function() {
+        console.log('did mount');
         emitter.on(EventTypes.SELECT_PREVIOUS_CONVERSATION, this._selectPreviousContact);
         emitter.on(EventTypes.SELECT_NEXT_CONVERSATION, this._selectNextContact);
     },
@@ -42,57 +50,66 @@ var ContactList = React.createClass({
         emitter.removeListener(EventTypes.SELECT_PREVIOUS_CONVERSATION, this._selectPreviousContact);
         emitter.removeListener(EventTypes.SELECT_NEXT_CONVERSATION, this._selectNextContact);
     },
-    render: function() {
-        var data = this.props.data;
-        if (!data || _.isEmpty(data)) {
-            return null;
-        }
-
-        data = _.groupBy(data, function(data) {
-            if (data.type === 'user') {
-                return data.name[0];
-            }
-            return data.type;
-        });
-
-        var contactList = _.map(data, function(data, key) {
-            var groupType = key !== 'group' ? 'user' : key;
-            return (
-                <ContactGroup
-                    /* key */
-                    key={prefix + key}
-                    /* props */
-                    index={key}
-                    data={data}
-                    groupName={Lang[key] || key}
-                    selectedIndex={this.state.selectedIndex}
-                    /* data-* attributes */
-                    groupType={groupType}
-                    /* event handler */
-                    onSelect={this._onSelect}
-                    />
-            );
-        }, this);
-
-        contactList = _.sortBy(contactList, function(contactGroup) {
-            var groupName = contactGroup.props.index;
-            if (groupName === 'group') {
-                return 0;
-            }
-            if (groupName === 'contact') {
-                return 1;
-            }
-            return String(groupName).charCodeAt(0);
-        });
-
+    renderGroupTitle: function (data, props, id) {
         return (
-            <ul className="contact-list"
-                style={makeStyle(style.conversationlist, this.props.style)}
+            <div
+                className={props.className + '-caption'}
+                style={props.style.caption}
                 >
-                {contactList}
-            </ul>
+                {Lang[id] || id}
+            </div>
         )
+    },
+    renderItem: function (data, props, id) {
+        var className = props.className;
+        var style = props.style;
+        return (
+            <li >
+                <Avatar
+                    className={className + '-avatar'}
+                    name={data.name}
+                    src={data.avatar}
+                    index={id}
+                    style={style.avatar}
+                    />
+
+                <div className={className + '-body'}>
+                    <div
+                        className={className + '-nickname'}
+                        style={makeStyle(style.title)}
+                        >
+                        {data.name}
+                    </div>
+                    <p
+                        className={className + '-last-appearance'}
+                        style={makeStyle(style.message)}
+                        >
+                        { data.count && (data.count + ' people')
+                        || (data.online && 'online' || (data.lastActiveTime + 'h ago'))}
+                    </p>
+                </div>
+            </li>
+        )
+    },
+    groupBy: function (data) {
+        if (data.type === 'user') {
+            return data.name[0];
+        }
+        return data.type;
     }
 });
 
-module.exports = ContactList;
+//private functions
+function defaultOnHoverIn(event) {
+    setStyle(
+        event.currentTarget.style,
+        style.contactlist.group.item.hover
+    );
+}
+
+function defaultOnHoverOut(event) {
+    setStyle(
+        event.currentTarget.style,
+        style.contactlist.group.item.default
+    );
+}
