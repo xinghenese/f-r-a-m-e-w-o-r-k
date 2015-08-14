8c98ab6ea7d52e327e7b5512cd997ff9cf9a6618
Kevin Wan <kevin@topcmm.com>
2015-08-14 15:44:17 +0800

    use HistoryMessages as the base class of GroupHistoryMessages and PrivateHistoryMessages

--- js/main/datamodel/grouphistorymessages.js
+++ js/main/datamodel/grouphistorymessages.js
@@ -12,71 +12,17 @@
 // dependencies
 var _ = require('lodash');
 var objects = require('../utils/objects');
-var MessageConstants = require('../constants/messageconstants');
-var Message = require('./message');
+var originify = require('../net/base/originify');
+var HistoryMessages = require('./historymessages');
 
 // exports
-function GroupHistoryMessages(data) {
-    this._requested = false;
-    this._data = data;
-    this._messages = _parseMessages(this._data["tms"]).reverse();
-}
+var GroupHistoryMessages = originify(HistoryMessages).extend({
+    init: function(data) {
+        HistoryMessages.call(this, data);
+    },
+    getGroupId: function() {
+        return parseInt(this._data["rid"]);
+    }
+});
 
 module.exports = GroupHistoryMessages;
-
-// module initialization
-GroupHistoryMessages.prototype.appendMessage = function(message) {
-    this._messages.push(message);
-};
-
-GroupHistoryMessages.prototype.prependMessages = function(messages) {
-    this._messages = messages.concat(this._messages);
-};
-
-GroupHistoryMessages.prototype.getGroupId = function() {
-    return parseInt(this._data["rid"]);
-};
-
-GroupHistoryMessages.prototype.getUnreadMessageCount = function() {
-    return parseInt(this._data["urc"] || 0);
-};
-
-GroupHistoryMessages.prototype.noMoreMessages = function() {
-    return objects.getBool(this._data["ise"]);
-};
-
-GroupHistoryMessages.prototype.getDirection = function() {
-    var direction = this._data["etp"];
-    return MessageConstants.parseMessageDirection(direction);
-};
-
-GroupHistoryMessages.prototype.isDirectionChanged = function() {
-    return "tp" in this._data;
-};
-
-GroupHistoryMessages.prototype.getReadCursor = function() {
-    return this._data["rcs"];
-};
-
-GroupHistoryMessages.prototype.getCleanCursor = function() {
-    return this._data["ccs"];
-};
-
-GroupHistoryMessages.prototype.getMessages = function() {
-    return this._messages;
-};
-
-GroupHistoryMessages.prototype.isRequested = function() {
-    return this._requested;
-};
-
-GroupHistoryMessages.prototype.setRequested = function() {
-    this._requested = true;
-};
-
-// private functions
-function _parseMessages(arr) {
-    return _.map(arr, function(v) {
-        return new Message(v);
-    });
-}
---
+++ js/main/datamodel/historymessages.js
@@ -0,0 +1,70 @@
+/**
+ * Created by kevin on 8/14/15.
+ */
+'use strict';
+
+// dependencies
+var _ = require('lodash');
+var Message = require('./message');
+
+// exports
+function HistoryMessages(data) {
+    this._requested = false;
+    this._data = data;
+    this._messages = _parseMessages(this._data["tms"] || []).reverse();
+}
+
+module.exports = HistoryMessages;
+
+// module initialization
+HistoryMessages.prototype.appendMessage = function(message) {
+    this._messages.push(message);
+};
+
+HistoryMessages.prototype.prependMessages = function(messages) {
+    this._messages = messages.concat(this._messages);
+};
+
+HistoryMessages.prototype.getUnreadMessageCount = function() {
+    return parseInt(this._data["urc"] || 0);
+};
+
+HistoryMessages.prototype.noMoreMessages = function() {
+    return objects.getBool(this._data["ise"]);
+};
+
+HistoryMessages.prototype.getDirection = function() {
+    var direction = this._data["etp"];
+    return MessageConstants.parseMessageDirection(direction);
+};
+
+HistoryMessages.prototype.isDirectionChanged = function() {
+    return "tp" in this._data;
+};
+
+HistoryMessages.prototype.getReadCursor = function() {
+    return this._data["rcs"];
+};
+
+HistoryMessages.prototype.getCleanCursor = function() {
+    return this._data["ccs"];
+};
+
+HistoryMessages.prototype.getMessages = function() {
+    return this._messages;
+};
+
+HistoryMessages.prototype.isRequested = function() {
+    return this._requested;
+};
+
+HistoryMessages.prototype.setRequested = function() {
+    this._requested = true;
+};
+
+// private functions
+function _parseMessages(arr) {
+    return _.map(arr, function(v) {
+        return new Message(v);
+    });
+}
