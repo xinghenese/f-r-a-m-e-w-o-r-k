dc4c5ddd177538176b935655df78b3737f8b92bf
Kevin Wan <kevin@topcmm.com>
2015-08-26 17:34:34 +0800

    remove redundant packages, close to minimize in mac

--- desktop.js
+++ desktop.js
@@ -21,9 +21,9 @@ var appIcon = null;
 app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    //if (process.platform != 'darwin') {
-    app.quit();
-    //}
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
 // This method will be called when Electron has finished
