f1a06685127b5410fac4cd074d0b1d84a16a8a48
sept <sept@liao.com>
2015-09-01 14:32:27 +0800

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 'use strict';
-
 var gulp = require('gulp');
-var packager = require('electron-packager');
-var isDev = process.env.NODE_ENV === 'development';
+var isDevelop = process.env.NODE_ENV === 'development';
 var isProduct = process.env.NODE_ENV === 'production';
 
-gulp.task('publish', ['build']);
+gulp.task('publish', ['build'], function () {
+    return gulp.src('./dist/main.js')
+        .pipe(require('gulp-uglify')())
+        .pipe(gulp.dest('./dist'))
+});
 
-gulp.task('clean', function(done) {
-    var del = require('del');
-    del(['dist'], done);
+gulp.task('clean', function (done) {
+    require('del')(['dist', 'chaoxin-*'], done);
 });
 
 // region bundle
@@ -24,41 +25,37 @@ var browserifyOptions = {
 };
 
 function bundlePayload(bundler) {
-    var glob = require('glob');
-    var source = require('vinyl-source-stream');
-    var plumber = require('gulp-plumber');
+    bundler.add(require('glob')('main/stores/*.js', {sync: true}));
 
-    bundler.add(glob('./js/main/stores/*.js', {sync: true}), {basedir: './'});
-
-    return function() {
+    return function () {
         return bundler.bundle()
-            .pipe(plumber(function(err) {
+            .pipe(require('gulp-plumber')(function (err) {
                 console.error(err);
                 this.emit('end');
             }))
-            .pipe(source('main.js'))
+            .pipe(require('vinyl-source-stream')('main.js'))
             .pipe(gulp.dest('./dist'));
     };
 }
 
 gulp.task('build', ['clean'], bundlePayload(require('browserify')(browserifyOptions)));
 
-gulp.task('watch', ['clean'], function() {
+gulp.task('watch', ['clean'], function () {
     var moment = require('moment');
-    var source = require('vinyl-source-stream');
     var watchify = require('watchify');
     var browserify = require('browserify');
     var bundler = watchify(browserify(browserifyOptions))
-        .on('error', function(err) {
+        .on('error', function (err) {
             console.error('err while watching');
             console.error(err);
             this.emit('end');
         })
-        .on('update', function() {
+        .on('update', function () {
             var label = moment().format('YYYY-MM-DD hh:mm:ss') + ' - Updated';
             console.time(label);
             bundle();
             console.timeEnd(label);
+            require('gulp-connect').reload();
         });
     var bundle = bundlePayload(bundler);
     return bundle();
@@ -66,7 +63,7 @@ gulp.task('watch', ['clean'], function() {
 // endregion
 
 // region tests
-gulp.task('tests', function(done) {
+gulp.task('tests', function (done) {
     var path = require('path');
     var jest = require('jest-cli');
     // see http://facebook.github.io/jest/docs/api.html#config-options
@@ -74,45 +71,46 @@ gulp.task('tests', function(done) {
     jest.runCLI(options, path.join(__dirname, 'js'), done);
 });
 
-gulp.task('tests:tdd', function() {
+gulp.task('tests:tdd', function () {
     gulp.watch('./js/**/*.js', ['tests']);
 });
 // endregion
 
-gulp.task('local-serve', ['watch'], function() {
-    return gulp.src('.')
-        .pipe(require('gulp-webserver')({
-            livereload: true
-        }));
-});
-
-gulp.task('package', ['build'], function() {
-    packager({
-        dir: ".",
-        name: "chaoxin",
-        platform: ["darwin", "win32"],
-        arch: "x64",
-        version: "0.31.0",
-        icon: "images/logomac.icns",
+// region desktop
+gulp.task('desktop:build', ['build'], function () {
+    require('electron-packager')({
+        dir: '.',
+        name: 'chaoxin',
         asar: true,
+        arch: 'x64',
+        icon: 'images/logomac.icns',
+        version: '0.31.1',
+        platform: ['darwin', 'win32'],
         overwrite: true,
         ignore: [
-            "node_modules/asar",
-            "node_modules/bower-resolve",
-            "node_modules/electron*",
-            "node_modules/gulp*",
-            "node_modules/jest*",
-            "node_modules/jscs",
-            "node_modules/vinyl-source-stream",
-            "node_modules/watchify",
-            "chaoxin-*"
+            'node_modules/asar',
+            'node_modules/bower-resolve',
+            'node_modules/electron*',
+            'node_modules/gulp*',
+            'node_modules/jest*',
+            'node_modules/jscs',
+            'node_modules/vinyl-source-stream',
+            'node_modules/watchify',
+            'chaoxin-*'
         ]
-    }, function(err, appPath) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log("done.\nthe package is located in: ", appPath);
-        }
+    }, function (err, appPath) {
+        if (err) console.error(err);
+        console.info('done.\nthe package is located in: ', appPath);
+    });
+});
+// endregion
+
+gulp.task('local-serve', ['watch'], function () {
+    require('gulp-connect').server({
+        root: '.',
+        port: 8000,
+        host: '0.0.0.0',
+        livereload: true
     });
 });
 
